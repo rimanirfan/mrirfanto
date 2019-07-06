1f27796ff2239a4ef062d40b50bdce9a46e0f471
rimanirfan <irfanriman@gmail.com>
2019-07-06 22:06:01 +0700

    render different components with react-router path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
     <div className="">
-      <About />
-      <Portfolio />
-      <Contact />
+      <h1>Home Page</h1>
+      <Router>
+        <div>
+          <Route path="/about" exact component={About} />
+          <Route path="/portfolio" exact component={Portfolio} />
+          <Route path="/contact" exact component={Contact} />
+        </div>
+      </Router>
     </div>
   );
 }
