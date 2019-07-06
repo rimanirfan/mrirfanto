573452b4982fc733c670d28d1be4a9eeb1cdbaf8
rimanirfan <irfanriman@gmail.com>
2019-07-06 22:18:20 +0700

    add Link to navigate to different path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import About from './components/About';
 import Portfolio from './components/Portfolio';
@@ -11,6 +11,11 @@ const App = () => {
       <h1>Home Page</h1>
       <Router>
         <div>
+          <ul>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/portfolio">Portfolio</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
           <Route path="/about" exact component={About} />
           <Route path="/portfolio" exact component={Portfolio} />
           <Route path="/contact" exact component={Contact} />
